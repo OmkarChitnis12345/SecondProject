1535453afa82d94a7ea8eec465b57319dd6cea0d
OmkarChitnis12345 <51022331+OmkarChitnis12345@users.noreply.github.com>
2019-06-02 12:02:03 +0530

    Add files via upload

--- two.php
+++ two.php
@@ -1,8 +1,16 @@
-<?php
-$age= array ("Peter => "35","Ben"=>"37", "Joe"=>"43");
 
-foreach($age  as  $x => $x_value){
-echo "Key".$x.",Value=".$x_value;
-echo"<br>";
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
 }
 ?>
+
+</body>
+</html>
