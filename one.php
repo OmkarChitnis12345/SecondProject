47ff1a325cb618cf2a9d8945e490369a4c48c064
OmkarChitnis12345 <51022331+OmkarChitnis12345@users.noreply.github.com>
2019-06-02 11:57:03 +0530

    Add files via upload

--- one.php
+++ one.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
 <?php
-$x=5;
-$y=10;
-function myTest(){
-global $x, $y;
-$y = $x +$y;
+$age= array ("Peter => "35","Ben"=>"37", "Joe"=>"43");
+
+foreach($age  as  $x => $x_value){
+echo "Key".$x.",Value=".$x_value;
+echo"<br>";
 }
-
-myTest();
-echo $y;
 ?>
-
-</body>
-</html>
